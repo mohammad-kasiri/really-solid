38f501ff7f337afca4468bdb9374831798478f30
mohammad <mohammad.kasirey@gmail.com>
2022-01-16 11:36:59 +0330

    installing task service provider

---
+++ Task/Providers/TaskServiceProvider.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Task\Providers;
+
+use Illuminate\Routing\Events\RouteMatched;
+use Illuminate\Support\ServiceProvider;
+
+use Illuminate\Support\Facades\Event;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Str;
+
+class TaskServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+
+    }
+
+    public function boot()
+    {
+        Event::listen(RouteMatched::class , function (RouteMatched $event){
+            Str::is('tasks.*' , $event->route->getName()) &&
+            !auth()->check() &&
+            redirect()->guest('login')->throwResponse();
+        });
+
+        Route::middleware('web')->group(base_path('Task/routes/routes.php'));
+        $this->loadMigrationsFrom([base_path('Task/migration')]);
+        $this->loadViewsFrom(base_path('Task/views') , 'Task');
+    }
+}

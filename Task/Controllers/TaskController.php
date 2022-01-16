bd25a558bd5d2f87f20d9346ca9a3fdfb5ab6723
mohammad <mohammad.kasirey@gmail.com>
2022-01-16 11:36:43 +0330

    make task controller

---
+++ Task/Controllers/TaskController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Task\Controllers;
+
+class TaskController
+{
+    public function index()
+    {
+
+    }
+
+    public function create()
+    {
+
+    }
+    public function sotre(){
+
+    }
+
+    public function edit()
+    {
+
+    }
+
+    public function update()
+    {
+
+    }
+    public function destroy()
+    {
+
+    }
+}
